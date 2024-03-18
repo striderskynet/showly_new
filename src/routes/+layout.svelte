d7209a270c1ce6a23a01cb4ec621c68ec2da1e22
Jose Ernesto Olazabal <os.nesty@gmail.com>
2024-03-18 08:55:50 -0400

    Title Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 	// console.log(data);
 </script>
 
+<svelteL:head>
+	<title>Showly 2.0</title>
+</svelteL:head>
+
 <div class="w-screen h-screen bg-black flex relative overflow-x-hidden">
 	<MenuLeft {data} />
 	<div class="sm:pl-16 w-full text-white"><slot /></div>
