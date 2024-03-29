d97d5de07d0a9205c59a2f0af0889c49a79f415a
Jose Ernesto Olazabal <os.nesty@gmail.com>
2024-03-28 21:07:20 -0400

    Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import '$assets/app.css';
+	import posthog from 'posthog-js';
+	import { onMount } from 'svelte';
 	import MenuLeft from './../components/UI/menu_left.svelte';
 	import TopNavigation from './../components/UI/top_navigation.svelte';
-
 	export let data;
+
+	onMount(() => {
+		posthog.init('phc_q48EhR7TH2EIKrHDjsm1RjXa0Y7Gl6Pv90ETCN7ET3Z', {
+			api_host: 'https://app.posthog.com',
+		});
+	});
 
 	// console.log(data);
 </script>
