740d258d00dc7394fe3fa6fa116d6fdfe4c23ec6
striderskynet <os.nesty@gmail.com>
2025-11-15 09:56:22 -0500

    [Fix] Cors

--- src/routes/show/[id]/[name]/+page.js
+++ src/routes/show/[id]/[name]/+page.js
@@ -1,10 +1,20 @@
 import cfg from '$config/main';
 export async function load({ fetch, params }) {
-    const show_fetcher = await fetch(cfg.api_show_id + params.id + "?append_to_response=credits,recommendations,videos,images,external_ids", cfg.api_options);
-    let show = await show_fetcher.json();
 
-    let torrent_fetcher = await fetch('https://eztvx.to/api/get-torrents?imdb_id=' + show.external_ids.imdb_id.replaceAll('tt', '') + '&limit=100')
-    show.torrent_list = await torrent_fetcher.json();
+    let show;
+    try {
+        const show_fetcher = await fetch(cfg.api_show_id + params.id + "?append_to_response=credits,recommendations,videos,images,external_ids", cfg.api_options);
+        show = await show_fetcher.json();
+    } catch (error) {
+
+    }
+
+    try {
+        let torrent_fetcher = await fetch('https://eztvx.to/api/get-torrents?imdb_id=' + show.external_ids.imdb_id.replaceAll('tt', '') + '&limit=100')
+        show.torrent_list = await torrent_fetcher.json();
+    } catch (error) {
+
+    }
 
     return {
         show: show,
