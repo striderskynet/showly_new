ff206b8bfdbb010c933ca5af30dbc97bb5be2d00
striderskynet <os.nesty@gmail.com>
2026-05-09 18:42:39 -0400

    [Added] Ficha tecnica

--- src/routes/truper/+page@.svelte
+++ src/routes/truper/+page@.svelte
@@ -505,6 +505,36 @@
 								>
 								{item.amount > 0 ? 'Añadir' : 'Sin Stock'}
 							</button>
+
+							<a
+								href={`https://www.truper.com/ficha_tecnica/controllers/index.php?codigo=${item.code}&origen=nal`}
+								target="_blank"
+								class="flex items-center justify-center w-full gap-2 py-1 text-[11px] text-slate-400 hover:text-red-400 transition-colors border border-transparent hover:border-red-900/30 rounded"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="14"
+									height="14"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									><path
+										d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"
+									/><polyline points="14 2 14 8 20 8" /><line
+										x1="16"
+										y1="13"
+										x2="8"
+										y2="13"
+									/><line
+										x1="16"
+										y1="17"
+										x2="8"
+										y2="17"
+									/><polyline points="10 9 9 9 8 9" /></svg
+								>
+								Ver Ficha Técnica (PDF)
+							</a>
 						</div>
 					</div>
 				</div>
